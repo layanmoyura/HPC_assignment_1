e69e72ce74ea615e61f81cf9fd1d544efeedf47a
layanmoyura <layanmoyura@gmail.com>
2023-07-02 09:17:46 +0530

    used numpy array in part one

--- part_1.py
+++ part_1.py
@@ -1,39 +1,25 @@
 import random
+import numpy as np
 from prettytable import PrettyTable
 
-math_sum = 0
-english_sum = 0
-dsa_sum = 0
-hpc_sum = 0
+num_students = 50
+num_subjects = 4
+
+marks = np.random.randint(0, 101, size=(num_students,num_subjects))
+subject_sums = np.sum(marks,axis=0)
+subject_averages = subject_sums/num_students
+
 marks_table = PrettyTable()
 marks_table.field_names = ["Student No.","Mathematics","English","Data Structures and Algorithms","High Performance Computing"]
-
-for student in range(1, 51):
-
-    math_marks = random.randint(0, 100)
-    english_marks = random.randint(0, 100)
-    dsa_marks = random.randint(0, 100)
-    hpc_marks = random.randint(0, 100)
-
-    math_sum += math_marks
-    english_sum += english_marks
-    dsa_sum += dsa_marks
-    hpc_sum += hpc_marks
-
-    marks_table.add_row([student, math_marks, english_marks, dsa_marks, hpc_marks])
-
-
-math_average = math_sum/50
-english_average = english_sum/50
-dsa_average = dsa_sum/50
-hpc_average = hpc_sum/50
+for student in range(num_students):
+    marks_table.add_row([student+1] + marks[student].tolist())
 
 average_table = PrettyTable()
 average_table.field_names = ["Subject","Average Marks"]
-average_table.add_row(["Mathematics",math_average])
-average_table.add_row(["English",english_average])
-average_table.add_row(["Data Structures and Algorithms",dsa_average])
-average_table.add_row(["High Performance Computing",hpc_average])
+average_table.add_row(["Mathematics",subject_averages[0]])
+average_table.add_row(["English",subject_averages[1]])
+average_table.add_row(["Data Structures and Algorithms",subject_averages[2]])
+average_table.add_row(["High Performance Computing",subject_averages[3]])
 
 print("Students' Marks:")
 print(marks_table)
