e5c0f4ac7bdbac20da956fbe116daaa4f7a12414
layanmoyura <layanmoyura@gmail.com>
2023-07-02 17:46:56 +0530

    remain:time improve

--- part_1.py
+++ part_1.py
@@ -1,33 +1,19 @@
-import random 
+import random
 import numpy as np
-from prettytable import PrettyTable
 import time
 
 time_start = time.time()
 
-num_students = 50
+num_students = 5000
 num_subjects = 4
-
 marks = np.random.randint(0, 101, size=(num_students,num_subjects))
-subject_sums = np.sum(marks,axis=0)
-subject_averages = subject_sums/num_students
+subjects = ["Mathematics","English","Data Structures and Algorithms","High Performance Computing"]
 
-marks_table = PrettyTable()
-marks_table.field_names = ["Student No.","Mathematics","English","Data Structures and Algorithms","High Performance Computing"]
-for student in range(num_students):
-    marks_table.add_row([student+1] + marks[student].tolist())
-
-average_table = PrettyTable()
-average_table.field_names = ["Subject","Average Marks"]
-average_table.add_row(["Mathematics",subject_averages[0]])
-average_table.add_row(["English",subject_averages[1]])
-average_table.add_row(["Data Structures and Algorithms",subject_averages[2]])
-average_table.add_row(["High Performance Computing",subject_averages[3]])
-
-print("Students' Marks:")
-print(marks_table)
-print("\nAverage marks per subject:")
-print(average_table)
+for subject_index in range(num_subjects):
+    local_data = marks[:,subject_index]
+    local_average = np.mean(local_data)
+    subject = subjects[subject_index]
+    print(f"Average of {subject}: >> {local_average}")
 
 time_end = time.time()
 
--- part_2.py
+++ part_2.py
@@ -10,24 +10,21 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-num_students = 50
+num_students = 5000
 num_subjects = 4
-
 marks = np.random.randint(0, 101, size=(num_students,num_subjects))
+subjects = ["Mathematics","English","Data Structures and Algorithms","High Performance Computing"]
+
 local_data = marks[:,rank]
 local_average = np.mean(local_data)
+subject = subjects[rank]
+print(f"Average of {subject}: >> {local_average}")
+
+comm.Barrier()
 
 if rank == 0:
-    print(f"Average of Mathematics: >> {local_average}")
-elif rank == 1:
-    print(f"Average of English: >> {local_average}")
-elif rank == 2: 
-    print(f"Average of Data Structures and Algorithms: >> {local_average}")
-elif rank == 3:
-    print(f"Average of High Performance Computing: >> {local_average}")
-else:
-    print("Error: Rank is not in range 0-3")
+    time_end = time.time()
+    print(f"Time taken: {time_end-time_start} seconds")
 
 
-time_end = time.time()
-print(f"Time taken: {time_end-time_start} seconds")
+
