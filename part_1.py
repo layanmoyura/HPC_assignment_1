8ffd62b9f1685d709ec99b3f09ab4d1bbe3932fa
layanmoyura <layanmoyura@gmail.com>
2023-07-05 01:01:09 +0530

    finished mod expected

--- part_1.py
+++ part_1.py
@@ -3,13 +3,13 @@ import numpy as np
 
 #time_start = time.time()
 
-num_students = 14000000
+num_students = 10000000
 num_subjects = 4
-marks = np.random.randint(0, 101, size=(num_students,num_subjects))
+marks = np.random.randint(0, 101, size=(num_subjects,num_students))
 subjects = ["Mathematics","English","Data Structures and Algorithms","High Performance Computing"]
 
 for subject_index in range(num_subjects):
-    local_data = marks[:,subject_index]
+    local_data = marks[subject_index,:]
     local_average = 0
     for student_index in range(num_students):
         local_average += local_data[student_index]
--- part_2.py
+++ part_2.py
@@ -4,16 +4,16 @@ from mpi4py import MPI
 
 #time_start = time.time()
 
-num_students = 14000000
+num_students = 10000000
 num_subjects = 4
-marks = np.random.randint(0, 101, size=(num_students,num_subjects))
+marks = np.random.randint(0, 101, size=(num_subjects,num_students))
 subjects = ["Mathematics","English","Data Structures and Algorithms","High Performance Computing"]
 
 # Initialize MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-local_data = marks[:,rank]
+local_data = marks[rank,:]
 local_average = 0
 for student_index in range(num_students):
        local_average += local_data[student_index]
