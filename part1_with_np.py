093110021be9cb5bc8803e7b6867bb5e3ec73d2f
layanmoyura <layanmoyura@gmail.com>
2023-07-06 15:26:55 +0530

    finished

--- part1_with_np.py
+++ part1_with_np.py
@@ -1,16 +1,12 @@
 import numpy as np
 
-num_students = 50
+num_students = 20000000
 num_subjects = 4
 marks = np.random.randint(0, 101, size=(num_subjects,num_students))
 subjects = ["Mathematics","English","Data Structures and Algorithms","High Performance Computing"]
 
 for subject_index in range(num_subjects):
     local_data = marks[subject_index,:]
-    local_average = 0
-    for student_index in range(num_students):
-        local_average += local_data[student_index]
-    local_average /= num_students
+    local_average = np.average(local_data)
     subject = subjects[subject_index]
     print(f"Average of {subject}: >> {local_average}")
-
--- part2_with_np.py
+++ part2_with_np.py
@@ -1,7 +1,7 @@
 import numpy as np
 from mpi4py import MPI
 
-num_students = 2000000
+num_students = 20000000
 num_subjects = 4
 marks = np.random.randint(0, 101, size=(num_subjects,num_students))
 subjects = ["Mathematics","English","Data Structures and Algorithms","High Performance Computing"]
@@ -10,14 +10,8 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 local_data = marks[rank,:]
-local_average = 0
-for student_index in range(num_students):
-       local_average += local_data[student_index]
-local_average /= num_students
-#local_average = np.average(local_data)
+local_average = np.average(local_data)
 subject = subjects[rank]
 print(f"Average of {subject}: >> {local_average}")
-
-
 
 
